b86c3a2cd3b19c7de611bf249ab1e13683697caa
devcui <devcui@outlook.com>
2021-08-19 14:36:23 +0800

    chore: fix links

--- packages/theme/src/router/optional-preloader.ts
+++ packages/theme/src/router/optional-preloader.ts
@@ -5,7 +5,7 @@ import { catchError } from 'rxjs/operators';
 import type { NzSafeAny } from 'ng-zorro-antd/core/types';
 
 /**
- * Optional pre-loading module, when it's necessary to load the resource at the first page load for some lazy routes, [example](https://github.com/hbyunzai/ng-alain/blob/master/src/app/routes/routes-routing.module.ts).
+ * Optional pre-loading module, when it's necessary to load the resource at the first page load for some lazy routes, [example](https://github.com/hbyunzai/ng-yunzai/blob/master/src/app/routes/routes-routing.module.ts).
  *
  * 可选预加载模块，当需要对某些懒路由在第一次页面加载时也一并加载该资源时，[示例](https://github.com/hbyunzai/ng-yunzai/blob/master/src/app/routes/routes-routing.module.ts)。
  *
