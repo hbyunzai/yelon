41bcc9bf97f7b70c50f12f975fdb5c2685204e51
cui <devcui@outlook.com>
2021-12-20 09:20:16 +0800

    fix(abc:lodop): update lodop.types.ts

--- packages/abc/lodop/lodop.types.ts
+++ packages/abc/lodop/lodop.types.ts
@@ -158,6 +158,14 @@ export interface Lodop extends CLodop {
     strHtml: string
   ): void;
 
+  ADD_PRINT_PDF(
+    Top: number | string,
+    Left: number | string,
+    Width: number | string,
+    Height: number | string,
+    strContent: string
+  ): void;
+
   /** 装载文档式模板 */
   ADD_PRINT_DATA(strDataStyle: string, varDataValue: NzSafeAny): void;
 
