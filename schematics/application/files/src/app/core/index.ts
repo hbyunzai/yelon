6f959744313eb5bf1a11e989fe1b5f1dce4a1b6f
devcui <devcui@outlook.com>
2022-10-06 01:11:06 +0000

    feat(cli): add dynamic start page

--- schematics/application/files/src/app/core/index.ts
+++ schematics/application/files/src/app/core/index.ts
@@ -1,4 +1,5 @@
-<% if (i18n) { %>export * from './i18n/i18n.service';<% } %>
+<% if (i18n) { %>export * from './i18n/i18n.service'; <% } %>
 export * from './module-import-guard';
 export * from './net/default.interceptor';
 export * from './startup/startup.service';
+export * from './start-page.guard';
