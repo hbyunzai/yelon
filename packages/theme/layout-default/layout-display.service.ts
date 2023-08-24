4eb15f2b4cccddaac579e6a54feae506c2e39086
devcui <devcui@outlook.com>
2023-08-24 11:56:32 +0800

    fix(layout): unuse bug

--- packages/theme/layout-default/layout-display.service.ts
+++ packages/theme/layout-default/layout-display.service.ts
@@ -1,4 +1,4 @@
-import {DestroyRef, inject, Injectable, OnDestroy} from '@angular/core';
+import {Injectable, OnDestroy} from '@angular/core';
 import {ActivatedRoute} from '@angular/router';
 import {BehaviorSubject, Subject, takeUntil} from 'rxjs';
 
