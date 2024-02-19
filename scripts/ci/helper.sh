362a58033c6724f05c6a4e48108d0d6f1585c3c0
devcui <devcui@outlook.com>
2024-02-19 13:48:03 +0800

    fix(ci): add CNAME copy

--- scripts/ci/helper.sh
+++ scripts/ci/helper.sh
@@ -4,3 +4,4 @@ git clone https://github.com/hbyunzai/archive-docs.git
 cp -r archive-docs/issue-helper src/dist/browser/issue-helper
 cp -r archive-docs/version src/dist/browser/version
 rm -rf archive-docs
+cp -rf CNAME src/dist/browser
