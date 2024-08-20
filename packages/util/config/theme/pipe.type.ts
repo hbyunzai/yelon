2f62723b2b711d9160051c62355d94a4fa386953
devcui <devcui@outlook.com>
2024-08-20 16:42:46 +0800

    feat(theme): support custom process of `_date` pipe (#1822)

--- packages/util/config/theme/pipe.type.ts
+++ packages/util/config/theme/pipe.type.ts
@@ -1,3 +1,10 @@
 export interface YunzaiThemePipeConfig {
   dateFormat?: string;
+  dateFormatCustom?: YunzaiThemePipeDateFormatCustom;
 }
+
+export type YunzaiThemePipeDateFormatCustom = (
+  value: Date,
+  formatString?: string | null,
+  options?: { locale?: Locale }
+) => string;
