e6d361210263b7bfcb8e204e1ca487f9cdebe8be
cui <devcui@outlook.com>
2025-02-12 11:42:01 +0800

    fix: fix util module missing export

--- packages/util/other/index.ts
+++ packages/util/other/index.ts
@@ -2,3 +2,6 @@ export * from './deep';
 export * from './logger';
 export * from './lazy.service';
 export * from './assert';
+export * from './path-to-regexp.service';
+export * from './resize';
+export * from './favicon';
