7b3be3cb107b1d79606a064abcc3d8950d3aa86c
devcui <devcui@users.noreply.github.com>
2025-02-13 07:48:06 +0000

    deploy: 7d9b4b7012c85a5e909d651002b402b39e890c22

---
+++ chunk-WUT4MLWW.js
@@ -0,0 +1,5 @@
+import{a as v}from"./chunk-Q6Z7JE74.js";import{E as I,G as N}from"./chunk-W6VIWJ3S.js";import{Ee as z,Hd as f,Ka as r,Kb as o,Ma as a,Ug as T,Vb as t,Vg as g,Xa as m,ha as n,jc as d,kc as u,wb as l,xb as s,yb as c}from"./chunk-SN7Y2XRP.js";var b=class p{meta=n(v);i18n=n(f);_full;set item(i){this._full=`${this.meta.github}/edit/master/${this.i18n.get(i.urls)}`}static \u0275fac=function(e){return new(e||p)};static \u0275cmp=m({type:p,selectors:[["edit-button"]],inputs:{item:"item"},decls:7,vars:4,consts:[["target","_blank","nz-tooltip","",1,"edit-button",3,"href","nzTooltipTitle"],["nzType","edit"]],template:function(e,h){e&1&&(t(0,`
+    `),l(1,"a",0),d(2,"i18n"),t(3,`
+      `),c(4,"nz-icon",1),t(5,`
+    `),s(),t(6,`
+  `)),e&2&&(a(),o("href",h._full,r),o("nzTooltipTitle",u(2,2,"app.content.edit-page")))},dependencies:[z,g,T,N,I],encapsulation:2})};export{b as a};
