a170b649c9c273c34e2d7becab60011a0a45ffa8
cui <devcui@outlook.com>
2025-07-09 14:43:36 +0800

    refactor(abc:global-footer): migrate to signals

--- packages/abc/global-footer/global-footer.types.ts
+++ packages/abc/global-footer/global-footer.types.ts
@@ -1,6 +1,10 @@
+import type { SafeHtml } from '@angular/platform-browser';
+
+import type { NzSafeAny } from 'ng-zorro-antd/core/types';
+
 export interface GlobalFooterLink {
-  [key: string]: any;
-  title: string;
+  [key: string]: NzSafeAny;
+  title: string | SafeHtml;
   href: string;
   blankTarget?: boolean;
 }
