e56b4c8e257f7f1019d3398612171a80e31a5467
cui <devcui@outlook.com>
2025-07-28 09:50:30 +0800

    chore: fix eslint

--- packages/abc/sv/sv.module.ts
+++ packages/abc/sv/sv.module.ts
@@ -6,7 +6,6 @@ import { NzOutletModule } from 'ng-zorro-antd/core/outlet';
 import { NzIconModule } from 'ng-zorro-antd/icon';
 import { NzTooltipModule } from 'ng-zorro-antd/tooltip';
 
-
 import { SVContainerComponent, SVTitleComponent } from './sv-container.component';
 import { SVValueComponent } from './sv-value.component';
 import { SVComponent } from './sv.component';
